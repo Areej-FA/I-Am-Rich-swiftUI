4ed350b2138227f7649c409aaaec448e239a9eaf
Areej-FA <46886699+Areej-FA@users.noreply.github.com>
2020-02-26 08:33:56 +0300

    Initial Commit

---
+++ I Am Rich swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich swiftUI
+//
+//  Created by Areej on 2/26/20.
+//  Copyright © 2020 Areej. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
